b80b5fc84905c96f827092121da7a1748aeb909f
srihan <bala.manukonda@gmail.com>
2018-09-16 21:43:54 -0500

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+//common place for all reducers
+import { combineReducers } from 'redux';
+import contactReducer from './contactReducer';
+
+export default combineReducers({
+  contact: contactReducer
+});
